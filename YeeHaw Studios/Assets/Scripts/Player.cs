650de4559194bcb4a4951454880355dd5cd9445a
Owen <arcanecogs@gmail.com>
2019-07-25 16:36:13 +1000

    Worked on AI scripts and player scripts, added the unity lockfile to git.ignore and added test objects to the technical scene
    Player controls are functionally done for the prototyping stage.
    AI patrols a set area by cycling though a list of target points, uses navmesh and navmesh agent.
    Added the unity lockfile to the ignore list to hopefully be able to submit to github while unity is open.

---
+++ YeeHaw Studios/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    public float horizontal;
+    public float vertical;
+
+    public float mouseSensitivity = 100/*0.5f*/;
+
+    float playerTranslation;
+
+    GameObject player;
+
+    private void Start()
+    {
+        player = gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationZ = Input.GetAxis("Vertical") * speed;
+        float translationX = Input.GetAxis("Horizontal") * speed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translationZ *= Time.deltaTime;
+        translationX *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(translationX, 0, translationZ);
+
+        // Rotate around our y-axis
+        //transform.Rotate(0, rotation, 0);
+        Rotation();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            player.transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        }
+        else
+        {
+            player.transform.position = new Vector3(player.transform.position.x, 1, player.transform.position.z);
+        }
+    }
+
+    void Rotation()
+    {
+        horizontal = Input.GetAxis("Mouse X") * mouseSensitivity;
+        vertical -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        vertical = Mathf.Clamp(vertical, -80, 80);
+
+        transform.Rotate(0, horizontal, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(vertical, 0, 0);
+    }
+}
---
+++ YeeHaw Studios/Assets/Scripts/Sentry.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Sentry : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent agent;
+    public Transform[] targetList;
+    public Transform currentTarget;
+    int lastTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        destination = agent.destination;
+
+        currentTarget = targetList[0];
+        lastTarget = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(destination, currentTarget.position) > 1.0f)
+        {
+            //destination = target.position;
+            destination = currentTarget.position;
+            agent.destination = destination;
+        }
+        else if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            if (currentTarget != targetList[targetList.Length-1])
+            {
+                lastTarget++;
+                currentTarget = targetList[lastTarget];
+            }
+            else
+            {
+                lastTarget = 0;
+                currentTarget = targetList[lastTarget];
+            }
+        }
+        //if (Vector3.Distance(destination, target.position) > 1.0f)
+         
+    }
+}
