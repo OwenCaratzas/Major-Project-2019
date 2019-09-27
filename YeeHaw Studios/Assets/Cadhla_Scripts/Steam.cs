b6539f5e093a383f97fa239d2ba2f77e4d08c4be
CadhlaRoberts <cadhlar@gmail.com>
2019-09-27 14:46:56 +1000

    Cadhla Update

---
+++ YeeHaw Studios/Assets/Cadhla_Scripts/Steam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steam : MonoBehaviour
+{
+
+    public GameObject steamOne;
+    public GameObject steamTwo;
+
+    public Animator m_robotAnimController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_robotAnimController.GetBool("Detected") == true && m_robotAnimController.GetBool("Patrol") == false)
+        {
+            steamOne.SetActive(true);
+            steamTwo.SetActive(true);
+        }
+        else
+        {
+            steamOne.SetActive(false);
+            steamTwo.SetActive(false);
+        }
+
+    }
+}
