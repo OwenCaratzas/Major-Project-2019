8308bd577e122b50b7781c746f768cee01af402c
Owen <arcanecogs@gmail.com>
2019-10-25 15:23:30 +1100

    Updated the proximity outline shader script

--- YeeHaw Studios/Assets/Scripts/ProximityHighlight.cs
+++ YeeHaw Studios/Assets/Scripts/ProximityHighlight.cs
@@ -6,28 +6,34 @@ public class ProximityHighlight : MonoBehaviour
 {
     private Material _material;
 
-    public Shader outlineShader;
+    //public Shader outlineShader;
 
-    public GameObject player;
+    public float outlineWidth;
 
     private Renderer _renderer;
+    public GameObject player;
+    private Player _playerScript;
     
     // Start is called before the first frame update
     void Start()
     {
         _renderer = GetComponent<Renderer>();
         _material = _renderer.material;
-        _material.shader = Shader.Find("Standard");
+        _playerScript = player.GetComponent<Player>();
+        //_material.shader = Shader.Find("Standard");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < 10)
+        if (Vector3.Distance(transform.position, player.transform.position) < _playerScript.InteractRange)
         {
-            _material.shader = outlineShader;
+            //_material.shader = outlineShader;
+            //_material.GetFloat("_Outline") = 0.025f;
+            _material.SetFloat("_Outline", outlineWidth);
         }
         else
-            _material.shader = Shader.Find("Standard");
+            //_material.shader = Shader.Find("Standard");
+            _material.SetFloat("_Outline", 0);
     }
 }
