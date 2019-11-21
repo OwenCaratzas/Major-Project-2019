e4ed3d44569f992dc3637c9cf2f84efdcc39f6f8
CadhlaRoberts <cadhlar@gmail.com>
2019-11-21 14:12:11 +1100

    Gold_Updates

---
+++ YeeHaw Studios/Assets/Cadhla_Scripts/SkyBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBox : MonoBehaviour
+{
+    public GameObject sky;
+
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sky.transform.position = Vector3.MoveTowards(startMarker.position, endMarker.position, Time.deltaTime/2);
+    }
+}
